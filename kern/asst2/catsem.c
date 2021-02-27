56064b59b95edb608ae2db92e243b0fca8a3f4ea
Muhammad Zaheer Hashmi <muhammad.hashmi@mail.utoronto.ca>
2021-02-26 22:23:13 -0500

    Implemented cat/mouse sem Alhamdullilah

--- kern/asst2/catsem.c
+++ kern/asst2/catsem.c
@@ -17,6 +17,23 @@
 #include <test.h>
 #include <thread.h>
 #include "catmouse.h"
+#include <synch.h>
+#include <machine/spl.h>
+
+
+/*GLOBALS*/
+struct semaphore* cat;
+struct semaphore* mouse;
+struct semaphore* _bowl;
+struct semaphore *bowl_1; 
+struct semaphore *bowl_2;
+struct semaphore *condstatus; 
+// bowl_status: 0 means free to use; 1 means occupied// 
+int bowl_status[2]= {0,0}; 
+//int cat_flag = 0;
+
+
+
 
 /*
  * 
@@ -44,6 +61,41 @@ void
 catsem(void * unusedpointer, 
        unsigned long catnumber)
 {
+        
+        int spl = splhigh();
+        if(mouse->count == 0){
+        splx(spl);
+        thread_yield(); }
+
+        P(_bowl); // Get the bowl 
+        P(cat);
+        int meals = 0;
+        //cat_flag = 1;
+        while(meals < NMEALS)
+        {
+                if(bowl_status[0]==0)
+                  {//P(bowl_1);
+                   bowl_status[0] = 1;
+                   catmouse_eat("cat",catnumber,1,meals);
+                   meals++;
+                   bowl_status[0] = 0;
+                 // V(bowl_2);    
+                 }
+
+                if(bowl_status[1] ==0 ){
+                 // P(bowl_2);
+                   bowl_status[1] = 1;
+                   catmouse_eat("cat",catnumber,2,meals);
+                   meals++;
+                   bowl_status[1] = 0;
+                 // V(bowl_2);
+                }
+     
+        }
+        
+        V(cat);
+        V(_bowl);
+      //  cat_flag = 0;
         /*
          * Avoid unused variable warnings.
          */
@@ -73,7 +125,47 @@ static
 void
 mousesem(void * unusedpointer, 
          unsigned long mousenumber)
-{
+{       
+        // Check if a cat is eating if it is dont approach bowl; no cat is eating go to bowl//
+
+        int spl = splhigh();
+        if(cat->count == 0){
+        splx(spl);
+        thread_yield(); }
+        
+        
+                P(_bowl);
+                P(mouse);
+
+                int meals = 0;
+                while(meals < NMEALS) {
+           
+             if(bowl_status[0] == 0){
+                //P(bowl_1);
+                bowl_status[0] = 1;
+                catmouse_eat("mouse",mousenumber,1,meals);
+                meals++;
+                bowl_status[0] = 0;
+              //  V(bowl_1);
+            }
+          
+
+             if( bowl_status[1] == 0)
+             {            
+             //  P(bowl_2);     
+                bowl_status[1] = 1;
+                catmouse_eat("mouse",mousenumber,2,meals);
+                meals++;
+                bowl_status[1] = 0;
+             //  V(bowl_2);
+             }
+  
+
+
+
+        }
+        V(mouse);
+        V(_bowl);
         /*
          * Avoid unused variable warnings.
          */
@@ -103,6 +195,13 @@ catmousesem(int nargs,
             char ** args)
 {
         int index, error;
+
+        cat = sem_create("cat",6);
+        mouse = sem_create("mouse",2);
+        _bowl = sem_create("bowl",2);
+        bowl_1= sem_create("bowl_1",1);
+        bowl_2 = sem_create("bowl_2",1);
+        condstatus = sem_create("condstatus",1);
    
         /*
          * Start NCATS catsem() threads.
